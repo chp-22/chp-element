eb0a1a4022a706c269a6767d683d2f7ed67e3691
chp-22 <2369092439@qq.com>
2024-02-20 15:07:49 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # chp-element
-平儿的组件库
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+平平创建的组件库
---
+++ src/views/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="my-button">{{ text }}</button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+    props: {
+      text: {
+        type: String,
+        default: 'Click me'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .my-button {
+    background-color: #0080ff;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  </style>
